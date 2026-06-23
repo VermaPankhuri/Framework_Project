912c00f40a57d4d728bd86e5608dd0b3d53381f5
Pankhuri Verma <vermapankhuri202429@gmail.com>
2026-06-23 07:40:52 +0530

    base class created

--- automation_project/src/main/java/base/Base.java
+++ automation_project/src/main/java/base/Base.java
@@ -1,12 +1,30 @@
 package base;
+import java.io.FileInputStream;
 
 public class Base {
 
-	public static void main(String[] args) {
-		System.out.println("hello ");
+	public static void main(String[] args) throws Exception{
+			Base base = new Base();
+		System.out.println(base.getUrl());
+		System.out.println(base.getBrowser());
 	}
-		// TODO Auto-generated method stub
+		public static WebDriver driver;
 
-	}
+public static WebDriver getDriver() {
+	return driver;
+}
+public static void setDriver(WebDriver driver1) {
+	driver = driver1;
+}
+public String getUrl() throws Exception{
+	Properties prop = new Properties();
+	prop.load(new FileInputStream ("C:\\Users\\Sachin.7.Yadav\\eclipse-workspace\\Framework_Practice\\src\\main\\java\\data.properties"));
+	return prop.getProperty("url");
+}
+public String getBrowser()throws Exception{
+	Properties prop = new Properties();
+	prop.load(new FileInputStream ("C:\\Users\\Sachin.7.Yadav\\eclipse-workspace\\Framework_Practice\\src\\main\\java\\data.properties"));
+	return prop.getProperty("browser");
+}
 
 }
